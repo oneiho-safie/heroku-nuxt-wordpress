a9af6069a77cdbb168e5322b7c754dff62ecf89d
onEihou <on@safie.jp>
2019-02-26 17:57:13 +0900

    test

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
-      <a class="navbar-brand" href="#">Safie</a>
+      <nuxt-link class="navbar-brand" to="/">Safie</nuxt-link>
       <button
         class="navbar-toggler"
         type="button"
@@ -16,7 +16,7 @@
       <div class="collapse navbar-collapse" id="navbarsExampleDefault">
         <ul class="navbar-nav mr-auto">
           <li class="nav-item active">
-            <a class="nav-link" href="/">Home <span class="sr-only">(current)</span></a>
+            <nuxt-link class="nav-link" to="/">Home</nuxt-link>
           </li>
           <li class="nav-item">
             <a class="nav-link" href="#">Link</a>
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -34,6 +34,7 @@
 import axios from 'axios'
 
 export default {
+  name: 'top',
   data() {
     return {
       news: [],

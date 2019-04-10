c9ba755bc10ea4dc430b8fa00fe30c4f4adfbe82
oneiho <on@safie.jp>
2019-04-10 17:00:31 +0900

    test

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -4,7 +4,7 @@
       <div class="container">
         <h1 class="display-3">Heroku + Firebase + WordPress mana******</h1>
         <p>This is a template for a simple marketing or informational website. It includes a large callout called a jumbotron and three supporting pieces of content. Use it as a starting point to create something more unique.</p>
-        <p><a class="btn btn-primary btn-lg" @click="test">Test &raquo;</a></p>
+        <p><a class="btn btn-primary btn-lg" @click="test">Test* &raquo;</a></p>
       </div>
     </div>
 
